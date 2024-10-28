9088b60929a6172274aee3eca1889d42b0ff2a51
fatimampg <fatimampgouveia@gmail.com>
2024-10-28 09:33:55 +0000

    feat(types): update types.d.ts

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,7 +1,16 @@
 export interface User {
-    id: string,
-    email: string,
-    createdAt: string,
-    password: string,
+  id: string;
+  email: string;
+  createdAt: string;
+  password: string;
 }
 
+export interface Product {
+  id: string;
+  title: string;
+  price: number;
+  category: string;
+  description?: string;
+  image?: string;
+  addedAt?: Date;
+}
