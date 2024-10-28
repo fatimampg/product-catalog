a56e1b00434c6946fe60df0bba792f6bb79fb545
fatimampg <fatimampgouveia@gmail.com>
2024-10-28 09:24:36 +0000

    chore: update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,26 @@
-import {nextui} from '@nextui-org/theme';
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/theme';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        'silver': '#CCCCCC',
-        'white-smoke': '#F2F2F2',
-        'dark-gray': '#7E7E7E',
-        'purple': '#7A306C',
+        background: '#ededed',
+        foreground: '#171717',
+        silver: '#CCCCCC',
+        whiteSmoke: '#F2F2F2',
+        darkGray: '#7E7E7E',
+        purple: '#7A306C',
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [nextui()],
 };
 export default config;
