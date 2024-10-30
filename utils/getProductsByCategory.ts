c50dea3006991e0baa35b20cc411743561885b8f
fatimampg <fatimampgouveia@gmail.com>
2024-10-30 00:54:21 +0000

    feat(utils): add functions to fetch women's products and men's products

--- utils/getProductsByCategory.ts
+++ utils/getProductsByCategory.ts
@@ -3,21 +3,21 @@
 import { Product } from '@/types/types';
 import { productsFakeStore, productsDummyJSON } from './getProducts';
 
-const catDummyJSON = [
-  'mens-shirts',
-  'mens-shoes',
-  'mens-watches',
-  'womens-bags',
-  'womens-dresses',
-  'womens-jewelley',
-  'womens-shoes',
-  'womens-watches',
-];
+// const catDummyJSON = [
+//   'mens-shirts',
+//   'mens-shoes',
+//   'mens-watches',
+//   'womens-bags',
+//   'womens-dresses',
+//   'womens-jewelley',
+//   'womens-shoes',
+//   'womens-watches',
+// ];
 
-const catFakeStore = ['jewelery', "men's clothing", "women's clothing"];
+// const catFakeStore = ['jewelery', "men's clothing", "women's clothing"];
 
-const WomensCategories = ['Clothing', 'Shoes', 'Bags', 'Watches and Jeweley'];
-const MensCategories = ['Clothing', 'Shoes', 'Accessories'];
+// const WomensCategories = ['Clothing', 'Shoes', 'Bags', 'Watches and Jeweley'];
+// const MensCategories = ['Clothing', 'Shoes', 'Accessories'];
 
 export const getWomensClothes = async () => {
   const womensClothing: Product[] = [];
@@ -49,72 +49,155 @@ export const getMensClothes = async () => {
   return mensClothing;
 };
 
-export const getShoes = async () => {
-  const shoes: Product[] = [];
+// export const getShoes = async () => {
+//   const shoes: Product[] = [];
 
+//   try {
+//     const formattedMensDummyJSON = await productsDummyJSON('mens-shoes');
+//     shoes.push(...formattedMensDummyJSON);
+
+//     const formattedWomensDummyJSON = await productsDummyJSON('womens-shoes');
+//     shoes.push(...formattedWomensDummyJSON);
+//   } catch (error) {
+//     console.log("Error - Couldn't fetch data from API");
+//   }
+//   return shoes;
+// };
+
+export const getWomensShoes = async () => {
+  const womensShoes: Product[] = [];
   try {
-    const formattedMensDummyJSON = await productsDummyJSON('mens-shoes');
-    shoes.push(...formattedMensDummyJSON);
-
     const formattedWomensDummyJSON = await productsDummyJSON('womens-shoes');
-    shoes.push(...formattedWomensDummyJSON);
+    womensShoes.push(...formattedWomensDummyJSON);
   } catch (error) {
     console.log("Error - Couldn't fetch data from API");
   }
-  return shoes;
+  return womensShoes;
 };
 
-export const getAccessories = async () => {
-  const accessories: Product[] = [];
+export const getMensShoes = async () => {
+  const mensShoes: Product[] = [];
+  try {
+    const formattedWomensDummyJSON = await productsDummyJSON('mens-shoes');
+    mensShoes.push(...formattedWomensDummyJSON);
+  } catch (error) {
+    console.log("Error - Couldn't fetch data from API");
+  }
+  return mensShoes;
+};
+
+// export const getAccessories = async () => {
+//   const accessories: Product[] = [];
+
+//   try {
+//     const formattedMensDummyJSON = await productsDummyJSON('mens-watches');
+//     accessories.push(...formattedMensDummyJSON);
+
+//     const formattedWBDummyJSON = await productsDummyJSON('womens-bags');
+//     accessories.push(...formattedWBDummyJSON);
+
+//     const formattedWJDummyJSON = await productsDummyJSON('womens-jewelley');
+//     accessories.push(...formattedWJDummyJSON);
+
+//     const formattedWWDummyJSON = await productsDummyJSON('womens-watches');
+//     accessories.push(...formattedWWDummyJSON);
+
+//     const formattedFakeStore = await productsFakeStore('jewelery');
+//     accessories.push(...formattedFakeStore);
+//   } catch (error) {
+//     console.log("Error - Couldn't fetch data from API");
+//   }
+//   return accessories;
+// };
+
+export const getWomensAccessories = async () => {
+  const womensAccessories: Product[] = [];
+
+  try {
+    const formattedWBDummyJSON = await productsDummyJSON('womens-bags');
+    womensAccessories.push(...formattedWBDummyJSON);
+
+    const formattedWJDummyJSON = await productsDummyJSON('womens-jewelley');
+    womensAccessories.push(...formattedWJDummyJSON);
+
+    const formattedWWDummyJSON = await productsDummyJSON('womens-watches');
+    womensAccessories.push(...formattedWWDummyJSON);
+
+    const formattedFakeStore = await productsFakeStore('jewelery');
+    womensAccessories.push(...formattedFakeStore);
+  } catch (error) {
+    console.log("Error - Couldn't fetch data from API");
+  }
+  return womensAccessories;
+};
+
+export const getMensAccessories = async () => {
+  const mensAccessories: Product[] = [];
 
   try {
     const formattedMensDummyJSON = await productsDummyJSON('mens-watches');
-    accessories.push(...formattedMensDummyJSON);
-
-    const formattedWBDummyJSON = await productsDummyJSON('womens-bags');
-    accessories.push(...formattedWBDummyJSON);
-
-    const formattedWJDummyJSON = await productsDummyJSON('womens-jewelley');
-    accessories.push(...formattedWJDummyJSON);
-
-    const formattedWWDummyJSON = await productsDummyJSON('womens-watches');
-    accessories.push(...formattedWWDummyJSON);
-
-    const formattedFakeStore = await productsFakeStore('jewelery');
-    accessories.push(...formattedFakeStore);
+    mensAccessories.push(...formattedMensDummyJSON);
   } catch (error) {
     console.log("Error - Couldn't fetch data from API");
   }
-  return accessories;
+  return mensAccessories;
 };
 
-export const fetchProducts = async () => {
-  const womensClothes: Product[] = await getWomensClothes();
-  const mensClothes: Product[] = await getMensClothes();
-  const shoes: Product[] = await getShoes();
+// export const fetchProducts = async () => {
+//   const womensClothes: Product[] = await getWomensClothes();
+//   const mensClothes: Product[] = await getMensClothes();
+//   const shoes: Product[] = await getShoes();
+//   const accesories: Product[] = await getAccessories();
 
-  const womensShoes = shoes.filter(
-    (product) => product.category === 'womens-shoes',
-  );
-  const mensShoes = shoes.filter(
-    (product) => product.category === 'mens-shoes',
-  );
-  const accesories: Product[] = await getWomensClothes();
-  const womensAccesories = accesories.filter((product) =>
-    ['womens-bags', 'womens-jewelley', 'womens-watches', 'jewelery'].includes(
-      product.category,
-    ),
-  );
-  const mensAccesories = accesories.filter(
-    (product) => product.category === 'mens-watches',
-  );
+//   const womensShoes = shoes.filter(
+//     (product) => product.category === 'womens-shoes',
+//   );
+//   const mensShoes = shoes.filter(
+//     (product) => product.category === 'mens-shoes',
+//   );
+//   const womensBags = accesories.filter((product) =>
+//     ['womens-bags'].includes(product.category),
+//   );
+//   const womensAccesories = accesories.filter((product) =>
+//     ['womens-jewelley', 'womens-watches', 'jewelery'].includes(
+//       product.category,
+//     ),
+//   );
+//   const mensAccesories = accesories.filter(
+//     (product) => product.category === 'mens-watches',
+//   );
+
+//   const womensProducts = [
+//     ...womensClothes,
+//     ...womensShoes,
+//     ...womensBags,
+//     ...womensAccesories,
+//   ];
+//   const mensProducts = [...mensClothes, ...mensShoes, ...mensAccesories];
+
+//   return { womensProducts, mensProducts };
+// };
+
+export const fetchWomensProducts = async () => {
+  const womensClothes: Product[] = await getWomensClothes();
+  const womensShoes: Product[] = await getWomensShoes();
+  const womensAccesories: Product[] = await getWomensAccessories();
 
   const womensProducts = [
     ...womensClothes,
     ...womensShoes,
     ...womensAccesories,
   ];
+
+  return { womensProducts };
+};
+
+export const fetchMensProducts = async () => {
+  const mensClothes: Product[] = await getMensClothes();
+  const mensShoes: Product[] = await getMensShoes();
+  const mensAccesories: Product[] = await getMensAccessories();
+
   const mensProducts = [...mensClothes, ...mensShoes, ...mensAccesories];
 
-  return { womensProducts, mensProducts };
+  return { mensProducts };
 };
